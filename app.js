0253bd6f87923c8d43ae155a9e7b0e805d10d791
Amin Ambike <113309178+AminAmbike@users.noreply.github.com>
2023-06-18 22:33:49 -0400

    Update app.js
    improved front-end/UI

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const app = express();
 
 const bodyParser = require("body-parser");
 
+const ejs = require("ejs")
+
+app.set('view engine', 'ejs')
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
 
 app.get("/", function(req,res) {
-    res.sendFile(__dirname +"/index.html")
+    // res.sendFile(__dirname +"/index.html")
+    res.render("index")
 });
 
 app.post("/", function(req,res) {
     req.body.cityName
 
-    const APIKey = "***********************";
+    const APIKey = "5db6b61aa79ddceacc340824e170fd4b";
     const unit = "metric";
     const query = req.body.cityName;
     const URL = "https://api.openweathermap.org/data/2.5/weather?&q=" + query + "&appid=" + APIKey + "&units=" + unit;
@@ -24,15 +30,25 @@ app.post("/", function(req,res) {
 
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
+            const temp = Math.round(weatherData.main.temp);
             const weatherDescription = weatherData.weather[0].description;
             console.log(weatherDescription);
+            const humidity = Math.round(weatherData.main.humidity);
+            const wind = Math.round(weatherData.wind.speed);
+            const feelslike = Math.round(weatherData.main.feels_like);
             const icon = weatherData.weather[0].icon;
             const imageURL = " https://openweathermap.org/img/wn/" + icon +"@2x.png"
-            res.write("<p>The weather is currently: " + weatherDescription + "<p>");
-            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
-            res.write("<image src=" + imageURL + ">");
-            res.send();
+            console.log(weatherData);
+            res.render("search", {
+                cityname: query,
+                temp: temp,
+                weatherDescription: weatherDescription,
+                icon: icon,
+                imageURL: imageURL,
+                wind: wind,
+                humidity: humidity,
+                feelslike: feelslike
+            });
         })
     });
 
